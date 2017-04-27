8eaba5f93aedba72e16cd97868c6324124fafeb6
William Bartlett <wbartlett@treeptik.fr>
2017-04-27 10:34:45 +0200

    Add Jenkinsfile

--- spring-petclinic-server/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ spring-petclinic-server/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -2,15 +2,21 @@ package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.samples.petclinic.config.PetclinicProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(PetclinicProperties.class)
-public class PetClinicApplication {
+public class PetClinicApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PetClinicApplication.class);
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(PetClinicApplication.class, args);
     }
 }
-
